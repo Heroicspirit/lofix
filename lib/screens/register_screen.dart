59844730a139cbe84672fd86fe1bf0aeea3ebd34
Heroicspirit <astarana21@gmail.com>
2025-12-05 17:38:39 +0545

    some change in code

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musicapp/screens/login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -131,7 +132,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
               height: 50,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context);
+                  Navigator.pushReplacement(
+                    context,
+                      MaterialPageRoute(builder: (context) => const LoginScreen()),
+                      );
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF6CA8E0),
