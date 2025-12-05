dae65552fbbc2b0b10d98728a5a43aaebc6786d8
Heroicspirit <astarana21@gmail.com>
2025-12-05 14:01:26 +0545

    Added Splash Screen code and continuing the work in onboarding Screen

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+//stless
+import 'package:flutter/material.dart';
+import 'package:musicapp/screens/splash_screen.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:SplashScreen(),
+    );
+  }
+}
